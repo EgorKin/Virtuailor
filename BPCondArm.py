c9b82dc725078b379d17809b4c239c72f10637c9
bill <b10902118@ntu.edu.tw>
2025-02-01 15:39:39 +0800

    clean up bp code

--- BPCondArm.py
+++ BPCondArm.py
@@ -14,22 +14,26 @@ def make_func(ea):
 def fix_arm_vtable(vfunc_addr):
     if not idc.is_code(vfunc_addr):
         code_err, func_err = make_func(vfunc_addr)
-        #if code_err == 0:
-            #print "Failed to create code, at", hex(vfunc_addr)
-        #elif not func_err:
-            #print "Failed to create function, at". hex(vfunc_addr)
+        if code_err == 0:
+            print("Failed to create code, at", hex(vfunc_addr))
+        elif not func_err:
+            print("Failed to create function, at". hex(vfunc_addr))
+
+def get_name(address):
+    name = idc.GetFunctionName(address)
+    if name == "":
+        name = idc.Name(address)
+        if name.startswith("_Z"):
+            name = idc.Demangle(name, 0)[:name.find("(")] # strip off the arguments
+    return name
 
 def get_fixed_name_for_object(address, prefix=""):
-    name = idc.GetFunctionName(int(address))
-    #print(prefix+" name @", address, name)
-    calc_func_name = hex(int(address) - base)[2:-1] #idc.SegStart(int(address))
-    if calc_func_name[-1] == "L":
-        calc_func_name = calc_func_name[:-1]
-    #v_func_name =  prefix + str(calc_func_name)
-    if name[:4] == "sub_":
-        name =  prefix + calc_func_name
-    elif name == "":
-        name =  prefix + calc_func_name
+    name = get_name(address)
+    if name[:4] == "sub_" or name == "loc_" or name == "":
+        addr_hex = hex(address - base)[2:-1] #idc.SegStart(int(address))
+        if addr_hex[-1] == "L":
+            addr_hex = addr_hex[:-1]
+        name =  prefix + addr_hex
     return name # nullsub_
 
 def get_vtable_and_vfunc_addr(is_brac, register_vtable, offset):
@@ -79,24 +83,13 @@ def add_all_functions_to_struct(start_address, struct_id, p_vtable_addr, offset)
             fix_arm_vtable(vtable_func_value)
         except:
             pass
-        #print("65: vtable_func_value:", hex(vtable_func_value))
-        v_func_name = idc.GetFunctionName(vtable_func_value)
-        #print("GetFunctionName:", v_func_name)
-        if not v_func_name :
+        v_func_name = get_fixed_name_for_object(vtable_func_value, "vfunc_")
+        if not v_func_name:
             print("GetFunctionName Error with", hex(vtable_func_value))
             print("bp address:", hex(bp_addr+base))
             print("vtable_addr", hex(p_vtable_addr))
             print("offset", offset)
             raise Exception("Error in adding functions to struct, at BP address::", hex(start_address))
-        #    vtable_func_value = idc.read_dbg_dword(vtable_func_value)  # Use dword for 32-bit
-        #    print("69: vtable_func_value:", vtable_func_value)
-        #    v_func_name = idc.GetFunctionName(vtable_func_value)
-        #    if v_func_name == '':
-        #        print("Error in adding functions to struct, at BP address::", hex(start_address))
-        v_func_name = get_fixed_name_for_object(int(vtable_func_value), "vfunc_")
-        #print("New v_func_name:", v_func_name)
-        #succ = idaapi.set_name(vtable_func_value, v_func_name, idaapi.SN_FORCE)
-        #print("func set_name:",vtable_func_value, succ)
         succ = idaapi.set_name(vtable_func_value, v_func_name, idaapi.SN_FORCE)
         #print("set func name " + v_func_name + " at " + hex(vtable_func_value),succ)
         err = idc.add_struc_member(struct_id, v_func_name, vtable_func_offset , idc.FF_DWRD, -1, 4)  # Use dword for 32-bit
@@ -119,52 +112,18 @@ def create_vtable_struct(start_address, vtable_name, p_vtable_addr, offset):
             print("Failed to create struct without name collision: " +  struct_name)
 
 def do_logic(virtual_call_addr, register_vtable, offset):
-    #is_brac_assign = idc.GetOpnd(int(idc.GetRegValue("pc")), 1).startswith('[')
-    #base = idc.SegStart(int(idc.GetRegValue("pc")))
     call_addr = virtual_call_addr + base
-    #print("base:", hex(base))
-    #print("call_addr:", hex(call_addr))
-    #is_brac_call = idc.GetOpnd(call_addr, 0).find('[')
     is_brac = -1
-    #if is_brac_assign != -1 and is_brac_call != -1:
-    #    is_brac = 0
     p_vtable_addr, v_func_addr = get_vtable_and_vfunc_addr(is_brac, register_vtable, offset)
-    #print("p_vtable_addr:", hex(p_vtable_addr), "v_func_addr:", hex(v_func_addr))
-    # possibly redundant
-    #v_func_name = get_fixed_name_for_object(v_func_addr, "vfunc_")
-    #idaapi.set_name(v_func_addr, v_func_name, idaapi.SN_FORCE)
-    # needed
     vtable_name = get_fixed_name_for_object(p_vtable_addr, "vtable_")
     idaapi.set_name(p_vtable_addr, vtable_name, idaapi.SN_FORCE)
-    try:
-        # add xref at blx
-        succ = idc.add_cref(call_addr, v_func_addr, idc.XREF_USER|idc.fl_CF) # blx register can be a far call
-        #print("add_cref",hex(call_addr), hex(v_func_addr), succ)
-    except:
-        print("Logging - xref failed to function at address:", hex(v_func_addr), ", from:", hex(v_func_addr) )
+    # add xref at blx
+    succ = idc.add_cref(call_addr, v_func_addr, idc.XREF_USER|idc.fl_CF) # blx register can be a far call
+    if not succ:
+        print("Logging - xref failed to function at address:" + hex(call_addr) + ", from:" + hex(v_func_addr) )
     # first arg for error message only, use 0 image base
     create_vtable_struct(virtual_call_addr, vtable_name, p_vtable_addr, offset)
-    #print("do_logic end")
 
-# not used by arm
-#if offset == "*":
-#    opnd2 = idc.GetOpnd(virtual_call_addr, 1)
-#    reg_offset = 0
-#    place = opnd2.find('+')
-#    if place != -1:
-#        sep = opnd2.find('*')
-#        if sep != -1:
-#            reg_offset = idc.GetRegValue(opnd2[place + 1: sep])
-#        register = opnd2[opnd2.find('[') + 1: place]
-#        if reg_offset:
-#            offset = opnd2[sep + 1: opnd2.find(']')]
-#            if offset.find('0x') != -1:
-#                int_offset = int(offset[offset.find('0x') +2:], 16)
-#            else:
-#                int_offset = int(offset)
-#            offset = int_offset * reg_offset
-#        else:
-#            offset = opnd2[place + 1: opnd2.find(']')]
 try:
     do_logic(virtual_call_addr, register_vtable, offset)
     # disable after cond executed
@@ -174,5 +133,5 @@ except Exception as e:
     print(e)
     import traceback
     traceback.print_exc()
-    print("Error! at BP address:", hex(idc.GetRegValue("pc")))
+    #print("Error! at BP address:", hex(idc.GetRegValue("pc")))
     return True
