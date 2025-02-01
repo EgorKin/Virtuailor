7cfc4f98606b986df3637b2e5ecc78823404c695
bill <b10902118@ntu.edu.tw>
2025-02-01 11:02:11 +0800

    fix print

--- BPCondArm.py
+++ BPCondArm.py
@@ -70,8 +70,8 @@ def add_all_functions_to_struct(start_address, struct_id, p_vtable_addr, offset)
         v_func_name = idc.GetFunctionName(vtable_func_value)
         #print("GetFunctionName:", v_func_name)
         if not v_func_name :
-            print("GetFunctionName Error with", vtable_func_value)
-            print("bp address:", hex(bp_addr))
+            print("GetFunctionName Error with", hex(vtable_func_value))
+            print("bp address:", hex(bp_addr+base))
             print("vtable_addr", hex(p_vtable_addr))
             print("offset", offset)
         #    vtable_func_value = idc.read_dbg_dword(vtable_func_value)  # Use dword for 32-bit
