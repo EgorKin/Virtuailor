20e1060ee7a5fbf499ca60d4005c3fcea10894c9
bill <b10902118@ntu.edu.tw>
2025-02-15 19:04:51 +0800

    split type and decl

--- BPCondArm.py
+++ BPCondArm.py
@@ -169,8 +169,10 @@ def create_vtable_struct(object_struct_name, vtable_struct_name, vtable_addr, of
             args = vfunc_type[arg_start_idx:vfunc_type.find(")")].split(",")
             if len(args) > 0:
                 args[0] = object_struct_name + " *this"
-                vfunc_type = vfunc_type[:arg_start_idx-1]+ " f(" + ",".join(args) + ")"
-                func_type_tuple = idc.parse_decl(vfunc_type, idc.PT_SILENT)
+                vfunc_type = vfunc_type[:arg_start_idx] + ",".join(args) + ")"
+                vfunc_decl = vfunc_type[:arg_start_idx-1]+ " f(" + ",".join(args) + ")"
+                print(vfunc_decl)
+                func_type_tuple = idc.parse_decl(vfunc_decl, idc.PT_SILENT)
                 idc.apply_type(vfunc_addr, func_type_tuple)
         fp_decl = to_func_ptr_decl(vfunc_type, vfunc_name)
         fp_decls.append(fp_decl)
