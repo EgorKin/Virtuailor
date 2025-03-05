dc2b898a8f0ef0835413d844fb01750be16096ff
bill <b10902118@ntu.edu.tw>
2025-03-06 00:10:26 +0800

    try rename obj gui

--- rename.py
+++ rename.py
@@ -61,9 +61,6 @@ def rename_object(obj_name, new_obj_name):
         cur = idc.get_next_func(cur)
 
 
-# TODO: rename object GUI
-
-
 def rename_func(func_addr, old_name, new_name):
     idc.set_name(func_addr, new_name)
     for id in range(idc.get_ordinal_qty()):
@@ -100,7 +97,7 @@ class RenameFunctionGUI(QtWidgets.QDialog):
             self.scope = ""
             self.basename = func_name
         else:
-            self.scope = func_name[:sep_idx]
+            self.scope = func_name[: sep_idx + 2]
             self.basename = func_name[sep_idx + 2 :]
 
         spacer = QtWidgets.QSpacerItem(
@@ -146,7 +143,7 @@ class RenameFunctionGUI(QtWidgets.QDialog):
         new_basename = self.func_name_area.text()
         if self.basename != new_basename:
             if self.scope:
-                new_name = self.scope + "::" + new_basename
+                new_name = self.scope + new_basename
             else:
                 new_name = new_basename
             print("Renaming `" + self.old_name + "` to `" + new_name + "`")
@@ -155,10 +152,78 @@ class RenameFunctionGUI(QtWidgets.QDialog):
         self.close()
 
 
+class RenameObjectGUI(QtWidgets.QDialog):
+    def __init__(self, ctx):
+        QtWidgets.QDialog.__init__(
+            self,
+            None,
+            QtCore.Qt.WindowSystemMenuHint
+            | QtCore.Qt.WindowTitleHint
+            | QtCore.Qt.WindowCloseButtonHint,
+        )
+
+        self.setWindowTitle("Rename Object")
+        self.setFixedWidth(600)
+        layout = QtWidgets.QVBoxLayout()
+
+        spacer = QtWidgets.QSpacerItem(
+            0, 8, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding
+        )
+
+        layout.addItem(spacer)
+        self.obj_name_area = QtWidgets.QLineEdit()
+        self.obj_name_area.setText("dummy")
+        self.obj_name_area.setSizePolicy(
+            QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Preferred
+        )
+        layout.addItem(self.obj_name_area)
+
+        # cannot register one widget twice, causing crash
+        spacer2 = QtWidgets.QSpacerItem(
+            0, 8, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding
+        )
+        layout.addItem(spacer2)
+
+        button_ok = QtWidgets.QPushButton("&OK")
+        button_ok.setDefault(True)
+        button_ok.setSizePolicy(
+            QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Preferred
+        )
+        button_ok.clicked.connect(self.on_ok_clicked)
+        button_layout = QtWidgets.QHBoxLayout()
+        button_layout.addStretch()
+        button_layout.addWidget(button_ok)
+        button_layout.addStretch()
+
+        layout.addLayout(button_layout)
+
+        self.setLayout(layout)
+
+    def on_ok_clicked(self):
+        # TODO
+        self.close()
+
+
+"""
+<class 'ida_kernwin.action_activation_ctx_t'>
+['__class__', '__del__', '__delattr__', '__dict__', '__doc__', '__format__', '__getattribute__', '__hash__', '__init__', '__module__', '__new__', '__reduce__',
+'__reduce_ex__', '__repr__', '__setattr__', '__sizeof__', '__str__', '__subclasshook__', '__swig_destroy__', '__weakref__', '_get_form_type', '_get_reg', 'action', 
+'chooser_selection', 'cur_ea', 'cur_enum', 'cur_extracted_ea', 'cur_fchunk', 'cur_flags', 'cur_func', 'cur_seg', 'cur_strmem', 'cur_struc', 'focus', 'form_type', 
+'has_flag', 'reg', 'reserved', 'reset', 'this', 'thisown', 'widget', 'widget_title', 'widget_type']
+"""
+
+
 def rename_function_gui(ctx):
-    print(type(ctx))
-    print(dir(ctx))
     gui = RenameFunctionGUI()
+    gui.exec_()
+
+
+def rename_object_gui(ctx):
+    print("Attributes of ctx object:")
+    for attr in dir(ctx):
+        if not attr.startswith("__"):
+            print(f"{attr}: {getattr(ctx, attr)}")
+    gui = RenameObjectGUI()
     gui.exec_()
 
 
@@ -173,7 +238,7 @@ utils.register_action(
 utils.register_action(
     "renameobject",
     "Rename object",
-    rename_function_gui,
+    rename_object_gui,
     None,
     [idaapi.BWN_LOCTYPS],
     True,
