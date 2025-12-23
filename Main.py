eef72d82c7e99308510f460415dc54f5bc125acf
Egor Kin <egorkin@gmail.com>
2025-12-23 14:08:27 +0300

    Update Main.py
    update to IDA 9.2

--- Main.py
+++ Main.py
@@ -1,6 +1,8 @@
 import idc
 import idautils
 import idaapi
+import ida_ida
+import ida_funcs
 
 idaapi.require("AddBP")
 idaapi.require("vtableAddress")
@@ -14,7 +16,7 @@ import GUI
 
 def get_all_functions():
     for func in idautils.Functions():
-        print(hex(func), idc.GetFunctionName(func))
+        print(hex(func), ida_funcs.get_func_name(func))
 
 
 def get_xref_code_to_func(func_addr):
@@ -22,8 +24,8 @@ def get_xref_code_to_func(func_addr):
     addr = {}
     for xref in a:
         frm = xref.frm  # ea in func
-        start = idc.GetFunctionAttr(frm, idc.FUNCATTR_START)  # to_xref func addr
-        func_name = idc.GetFunctionName(start)  # to_xref func name
+        start = idc.get_func_attr(frm, idc.FUNCATTR_START)  # to_xref func addr
+        func_name = ida_funcs.get_func_name(start)  # to_xref func name
         addr[func_name] = [xref.iscode, start]
     return addr
 
@@ -34,21 +36,20 @@ def add_bp_to_virtual_calls(cur_addr, end):
     while cur_addr < end:
         if cur_addr == idc.BADADDR:
             break
-        elif idc.GetMnem(cur_addr) == CALL_INSTR:
-            operand = idc.GetOpnd(cur_addr, 0)
-            # print(
-            #    "Virtual Call " + call_instr + " " + operand + " at: " + hex(cur_addr)
-            # )
-            # print("reg0", idc.GetOpnd(cur_addr, 0))
+        elif idc.print_insn_mnem(cur_addr) == CALL_INSTR:
+            operand = idc.print_operand(cur_addr, 0)
+
+            print("Virtual Call " + str(CALL_INSTR) + " " + str(operand) + " at: " + hex(cur_addr))
+            # print("reg0", idc.print_operand(cur_addr, 0))
             # if True in [
-            #    idc.GetOpnd(cur_addr, 0).find(reg) != -1 for reg in registers
+            #    idc.print_operand(cur_addr, 0).find(reg) != -1 for reg in registers
             # ]:  # call involving a register, but we can't handle operations on the register
-            if idc.GetOpnd(cur_addr, 0) in REGS:  # ensure a register only call
+            if idc.print_operand(cur_addr, 0) in REGS:  # ensure a register only call. может быть можно было idc.get_operand_type(cur_addr, 0) == idaapi.o_reg:
                 cond, bp_address = vtableAddress.write_vtable2file(cur_addr, operand)
                 if cond != "":
                     bp_vtable = AddBP.add(bp_address, cond)
-                    # print("BP added at: ", hex(bp_address))
-        cur_addr = idc.NextHead(cur_addr)
+                    print("BP added at: ", hex(bp_address))
+        cur_addr = idc.next_head(cur_addr)
 
 
 def set_values(start, end):
@@ -66,12 +67,8 @@ def to_hex_str(num):
 
 
 if __name__ == "__main__":
-    start_addr = (
-        idc.MinEA()  # 0x20FE74  # You can change the virtual calls address range
-    )
-    end_addr = idc.MaxEA()  # 0x20FE7A
-    start_addr_str = hex(start_addr)
-    end_addr_str = hex(end_addr)
+    start_addr = ida_ida.inf_get_min_ea()  # You can change the virtual calls address range
+    end_addr = ida_ida.inf_get_max_ea()
     oldTo = idaapi.set_script_timeout(0)
     # Initializes the GUI: Deletes the 0x in the beginning and the L at the end:
     gui = GUI.VirtuailorBasicGUI(
